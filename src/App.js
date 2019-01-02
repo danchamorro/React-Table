b91554602e40b56fba5d8e38a6bb73ec039649da
Dan Chamorro <dan@danchamorro.com>
2019-01-02 00:12:14 -0500

    Displaying Data
    Displaying post data in table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class App extends Component {
   state = {
@@ -15,9 +16,28 @@ class App extends Component {
   }
 
   render() {
+    const columns = [
+      {
+        Header: "User ID",
+        accessor: "userId"
+      },
+      {
+        Header: "ID",
+        accessor: "id"
+      },
+      {
+        Header: "Title",
+        accessor: "title"
+      },
+      {
+        Header: "Content",
+        accessor: "body"
+      }
+    ];
     return (
       <div className="App">
         <h1>React Table Test</h1>
+        <ReactTable columns={columns} data={this.state.posts} />
       </div>
     );
   }
